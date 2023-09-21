38b5f3ee1d9db29c3e5c6bc263e5c1566a8c558f
joefioresi718 <joefioresi718@gmail.com>
2023-09-21 09:35:49 -0400

    add filenames

--- initial_analysis.py
+++ initial_analysis.py
@@ -2,14 +2,17 @@ import pandas as pd
 import glob
 
 
-acs_files = glob.glob('data/ACS*.csv')
+acs_files = sorted(glob.glob('data/ACS*.csv'))
 
 data_dictionary = pd.read_excel('data/ACS Data Dictionary.xlsx')
 print(data_dictionary.head())
+def_names = {r['Variable']: r['ACS Definition'] for i, r in data_dictionary.iterrows()}
+print(def_names)
 
 var_list = data_dictionary['Variable'].tolist()
  
 for filename in acs_files:
     df = pd.read_csv(filename)
-    print(df.head())
-    exit()
+    df.rename(columns=def_names, inplace=True)
+    df.to_csv(filename.replace('data', 'clean_data'), index=False)
+    # print(df.head())
